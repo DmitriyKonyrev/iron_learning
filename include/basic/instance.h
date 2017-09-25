674c41d2d6fd5c52fc75f02d3ef28357e026a31a
d.konyrev <dmitriykonyrev@yandex.ru>
2017-09-25 21:05:06 +0300

    IL-111: [+] basic: add instance and pool classes

---
+++ include/basic/instance.h
@@ -0,0 +1,46 @@
+#ifndef INSTANCE_H
+#define INSTANCE_H
+
+#include <math/mathvector.h>
+
+#include <functional>
+#include <vector>
+#include <string.h>
+
+using namespace MathCore::AlgebraCore::VectorCore;
+
+namespace MachineLearning
+{
+
+	class Instance
+	{
+	private:
+
+		std::reference_wrapper<MathVector<double>> features;
+		double goal;
+
+	public:
+		Instance(MathVector<double>& _features, double _goal)
+			: features(_features), goal(_goal)
+		{
+		}
+
+
+		double& operator [](int index) const;
+
+		double getGoal() const;
+
+		MathVector<double>& getFeatures() const;
+
+		operator MathVector<double>&();
+
+		operator double() const;
+
+		size_t getFeaturesSize();
+
+		size_t getNotNullFeaturesSize();
+
+	};
+};
+
+#endif
---
+++ src/basic/instance.cpp
@@ -0,0 +1,47 @@
+#include <vector>
+#include <string.h>
+
+#include <basic/instance.h>
+#include <math/mathvector.h>
+
+using namespace  MathCore::AlgebraCore::VectorCore;
+
+namespace MachineLearning
+{
+
+	double& Instance::operator [](int index) const
+	{
+		return *(new double(features.get().getElement((size_t)index)));
+	}
+
+	double Instance::getGoal() const
+	{
+		return goal;
+	}
+
+	MathVector<double>& Instance::getFeatures() const
+	{
+		return features.get();
+	}
+
+
+	Instance::operator MathVector<double>&()
+	{
+		return features.get();
+	}
+
+	Instance::operator double() const
+	{
+		return this->goal;
+	}
+
+	size_t Instance::getFeaturesSize()
+	{
+		return this->features.get().getSize();
+	}
+
+	size_t Instance::getNotNullFeaturesSize()
+	{
+		return this->features.get().getSizeOfNotNullElements();
+	}
+}
